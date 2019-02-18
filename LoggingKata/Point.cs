ab8169683f1d6f830a8f2a4b367b1f1f8d68002c
knightanoleiii@gmail.com <knightanoleiii@gmail.com>
2019-02-18 08:18:55 -0600

    Hamilton and Athens are the furthest two Taco Bells from eachother.

--- LoggingKata/Point.cs
+++ LoggingKata/Point.cs
@@ -1,10 +1,17 @@
 ﻿namespace LoggingKata
 {
+    /// <summary>
+    /// a coordinate point containing two doubles
+    /// </summary>
     public struct Point
     {
         public double Longitude { get; set; }
         public double Latitude { get; set; }
-        public Point(double x, double y) { this.Latitude = x; this.Longitude = y; }
-    }
 
+        public Point(double x, double y)
+        {
+            this.Latitude = x;
+            this.Longitude = y;
+        }
+    }
 }
--- LoggingKata/Program.cs
+++ LoggingKata/Program.cs
@@ -15,13 +15,51 @@ namespace LoggingKata
             logger.LogInfo("Log initialized");
 
             var lines = File.ReadAllLines(csvPath);
+            
             //format each line in lines into line[0] = longitude, line[1] = latitude, line[2] = location name
-            var frmtlines = lines.Select(x => x.Replace("/", "").Split("...")[0].Split(','));
-            var parser = new TacoParser();
+            var frmtlines = lines.Select(ln => ln.Replace("/", "").Split("...")[0].Split(','));
             
+            foreach( string[] line in frmtlines )
+            {
+                TacoBell.Add(TacoBell.ParseLine(line));
+            }
+
+            double longest = 0.0;
+            TacoBell[] furthestTwo = new TacoBell[2];
+            foreach( var taco in TacoBell.GetTacoBells )
+            {
+                foreach( var otherTaco in TacoBell.GetTacoBells )
+                {
+                    double comp = taco.Coordinate.GetDistanceTo(otherTaco.Coordinate);
+                    if( comp > longest )
+                    {
+                        furthestTwo[0] = taco;
+                        furthestTwo[1] = otherTaco;
+                        longest = comp;
+                    }
+                }
+
+            }
+
+            logger.LogInfo($"TacoBells with the longest distance apart are: {furthestTwo[0].Name} and {furthestTwo[1].Name}");
+
             
-            // TODO:  Find the two Taco Bells that are the furthest from one another.
-            // HINT:  You'll need two nested forloops
+
+            
+
+            Console.WriteLine("Would you like to see the TacoBells?");
+            while( Console.ReadKey().KeyChar.ToString().ToUpper() == "Y" )
+            {   
+                TacoBell.ConsoleWriteAllBells();
+                Console.WriteLine("Would you like to see the TacoBells again?");
+            }
+            }
+
+        ~Program() {
+            Console.ReadLine();
+
+            
         }
     }
+    
 }
